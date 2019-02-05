c54a061771461dfa7fadedb75af70d0efa94ba52
matheuseduardos <matheus.eduardo7890@gmail.com>
2019-02-05 15:19:51 -0200

    Fix database access.

--- src/main/kotlin/org/elemental/databases/HikariCustomConfig.kt
+++ src/main/kotlin/org/elemental/databases/HikariCustomConfig.kt
@@ -13,9 +13,8 @@ object HikariCustomConfig {
 
     private fun postgreSqlTemplate(): HikariDataSource {
         val hikariDS = HikariDataSource()
-        println(System.getenv("DATABASE_URL"))
-        hikariDS.dataSourceClassName = "org.postgresql.ds.PGPoolingDataSource"
-        hikariDS.jdbcUrl = "postgres://ctevovcnghkgof:80bb5353ee8ee495d86a9059b922bc07f9db96f6407d053e2bccd92f877e9df8@ec2-107-21-224-76.compute-1.amazonaws.com:5432/d2040b14djuqeh"
+        hikariDS.dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
+        hikariDS.jdbcUrl = System.getenv("DATABASE_URL")
         return hikariDS
     }
 }
